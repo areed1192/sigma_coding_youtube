74f2cbabe3c1997a5d335a299878fc801deddb75
Alex Reed <alexreed1192@gmail.com>
2019-03-25 14:07:05 -0700

    Update datetimes.py

--- python/python-core/datetimes.py
+++ python/python-core/datetimes.py
@@ -22,3 +22,22 @@ print(date_stamp)
 print(date_stamp.year)
 print(date_stamp.month)
 print(date_stamp.day)
+
+# ------------------------- PART TWO --------------------------
+
+from datetime import datetime, date, time
+
+# create a date and a time
+my_date = date(2019, 3, 22)
+my_time = time(12, 30)
+
+# create a datetime
+my_datetime = datetime.combine(my_date, my_time)
+print(my_datetime)
+
+# get the different components
+print(my_datetime.year)
+print(my_datetime.month)
+print(my_datetime.day)
+print(my_datetime.hour)
+print(my_datetime.minute)
