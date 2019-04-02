5cd7f30250add8a11740c101d8f09f8542ef63bf
Alex Reed <alexreed1192@gmail.com>
2019-04-01 20:31:16 -0700

    Create loops.py

---
+++ python/python-core/loops.py
@@ -0,0 +1,56 @@
+primes = [2, 3, 5, 7]
+for prime in primes:
+    print(prime)
+
+# Prints out the numbers 0,1,2,3,4
+for x in range(5):
+    print(x)
+
+# Prints out 3,4,5
+for x in range(3, 6):
+    print(x)
+
+# Prints out 3,5,7
+for x in range(3, 8, 2):
+    print(x)
+
+
+# Prints out 0,1,2,3,4
+count = 0
+while count < 5:
+    print(count)
+    count += 1  # This is the same as count = count + 1'
+
+
+# Prints out 0,1,2,3,4
+count = 0
+while True:
+    print(count)
+    count += 1
+    if count >= 5:
+        break
+
+# Prints out only odd numbers - 1,3,5,7,9
+for x in range(10):
+    # Check if x is even
+    if x % 2 == 0:
+        continue
+    print(x)
+
+
+# Prints out 0,1,2,3,4 and then it prints "count value reached 5"
+
+count=0
+while(count<5):
+    print(count)
+    count +=1
+else:
+    print("count value reached %d" %(count))
+
+# Prints out 1,2,3,4
+for i in range(1, 10):
+    if(i%5==0):
+        break
+    print(i)
+else:
+    print("this is not printed because for loop is terminated because of break but not due to fail in condition")
