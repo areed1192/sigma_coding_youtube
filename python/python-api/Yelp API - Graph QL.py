962cddac24839a00ea67eeb328c33a72fc765412
Alex Reed <alexreed1192@gmail.com>
2019-05-08 14:09:18 -0700

    Update Yelp API - Graph QL.py

--- python/python-api/Yelp API - Graph QL.py
+++ python/python-api/Yelp API - Graph QL.py
@@ -1,28 +1,21 @@
+# ---- pip install gql ----
 
-# Import Modules
+# import our modules
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
+from config import api_key
 
+# define our authentication process.
+header = {'Authorization': 'bearer {}'.format(api_key),
+          'Content-Type':"application/json"}
 
-# Define API Key, Search Type, and header
-MY_API_KEY = '<YOUR API KEY>'
-BUSINESS_PATH = 'https://api.yelp.com/v3/graphql'
-HEADERS = {'Authorization': 'bearer %s' % MY_API_KEY}
+# Build the request framework
+transport = RequestsHTTPTransport(url='https://api.yelp.com/v3/graphql', headers=header, use_json=True)
 
-# Build the URL
-_transport = RequestsHTTPTransport(
-    url='https://api.yelp.com/v3/graphql',
-    headers = HEADERS,
-    use_json=True
-)
-
-# Define the client - The Part the Reaches out to the Server
-client = Client(
-    transport=_transport,
-    fetch_schema_from_transport=True,
-)
-
-# Define The Query
+# Create the client
+client = Client(transport=transport, fetch_schema_from_transport=True)
+        
+# define a simple query
 query = gql('''
 {
   business(id: "garaje-san-francisco") {
@@ -40,5 +33,105 @@ query = gql('''
 }
 ''')
 
-# Print the Results
+# execute and print this query
+print('-'*100)
+print(client.execute(query))
+
+
+# define a simple query - with nested parameters
+query = gql('''
+{
+  business(id: "garaje-san-francisco") {
+    name
+    id
+    is_claimed
+    is_closed
+    url
+    phone
+    display_phone
+    review_count
+    rating
+    photos
+    coordinates {                      
+            latitude                      
+            longitude                     
+        }
+    }
+}
+''')
+
+# execute and print the query
+print('-'*100)
+print(client.execute(query))
+
+
+# define a query for multiple businesses - no fragments
+query = gql('''{
+    b1: business(id: "yelp-san-francisco") {
+        name
+        id
+        rating
+        review_count
+        photos
+    }
+    b2: business(id: "garaje-san-francisco") {
+        name
+        id
+        rating
+        review_count
+        photos
+    }
+}
+''')
+
+# execute and print the query
+print('-'*100)
+print(client.execute(query))
+
+
+# define a query for multiple businesses - with fragments
+query = gql('''{
+    b1: business(id: "yelp-san-francisco") {
+        ...basicBizInfo
+    }
+    b2: business(id: "garaje-san-francisco") {
+        ...basicBizInfo
+    }
+}
+fragment basicBizInfo on Business {
+    name
+    id
+    rating
+    review_count
+    photos
+    coordinates {                      
+            latitude                      
+            longitude                     
+        }
+}
+''')
+
+# execute and print the query
+print('-'*100)
+print(client.execute(query))
+
+
+# define a query with a different endpoint, in this case reviews
+query = gql('''{
+  reviews(business: "garaje-san-francisco") {
+    total
+    review {
+      rating
+      text
+      user {
+        name
+        image_url
+      }
+    }
+  }
+}
+''')
+
+# execute and print the query
+print('-'*100)
 print(client.execute(query))
